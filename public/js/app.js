7a08b45e8ad3f10e91076fe119576d9443db110e
Adam Napora <anapora@apple.com>
2015-05-10 17:34:54 +0100

    made socker url dynamic!!! small win!

--- public/js/app.js
+++ public/js/app.js
@@ -5,14 +5,11 @@ angular.module('app', [
         'btford.socket-io',
         'swipe'
     ]).
-    constant('SOCKET_PROTOCOL', 'ws').
-    constant('API_ADDRESS', 'localhost').
-    constant('API_PORT', '3001').
-    factory('mySocket', ['socketFactory', 'SOCKET_PROTOCOL', 'API_ADDRESS', 'API_PORT', function (socketFactory,
-        SOCKET_PROTOCOL, API_ADDRESS, API_PORT) {
+    constant('SOCKET_ADDRESS', location.origin.replace(/^http/, 'ws')).
+    factory('mySocket', ['socketFactory', 'SOCKET_ADDRESS', function (socketFactory, SOCKET_ADDRESS) {
 
         return socketFactory({
-            ioSocket: io.connect(SOCKET_PROTOCOL + '://' + API_ADDRESS + ':' + API_PORT)
+            ioSocket: io.connect(SOCKET_ADDRESS)
         });
 
     }]).controller('AppController', ['$scope', 'mySocket', function ($scope, mySocket) {
